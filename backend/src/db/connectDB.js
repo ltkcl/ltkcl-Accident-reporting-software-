7367d0777261fedcb06007f05967c122fe7abb04
Kishore Chandra Lenka <kishore22j@gmail.com>
2026-06-20 14:48:38 +0530

    Added vercel config file2

--- backend/src/db/connectDB.js
+++ backend/src/db/connectDB.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 async function connectDB(){
     try {
-        const connectionInstance =await mongoose.connect(`${process.env.MONGODB_URI}/${process.env.MONGODB_NAME}`);
+        const connectionInstance =await mongoose.connect(`${process.env.PROD_MONGODB_URI}/${process.env.MONGODB_NAME}`);
         console.log(`Connected to the MongoDB!! `,connectionInstance.connection.host);
     } catch (error) {
         console.log("Failed to connect with mongoDB ",error);
